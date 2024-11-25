9d06c16764da80f3e8cc54abed19da280cca34ed
PrymetymeGaming <108591330+PrymetymeGaming@users.noreply.github.com>
2024-11-25 14:23:23 -0800

    Update db_connection.php
    updated to environmental username/pass

--- thj/includes/db_connection.php
+++ thj/includes/db_connection.php
@@ -1,10 +1,11 @@
 <?php
-// Database connection settings
-$host = 'ip:port';
-$dbname = 'dbname';
-$username = 'username';
-$password = 'password';
+// Load environment variables
+$host = getenv('DB_HOST') ?: 'localhost'; // Default to localhost if not set
+$dbname = getenv('DB_NAME') ?: 'content';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: '';
 
+// Database connection
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
